5a77327dd0b7404c340b54953566b9a631086166
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2023-02-28 12:46:14 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,7 @@
+# {connectorName} persons PowerShell script
+# This script retrieves persons data from a source system
 
+# Some additional code you want to include in all generated scripts
+$global:verbosePreference = 'Continue'
+
+# Your code to retrieve persons data goes here
